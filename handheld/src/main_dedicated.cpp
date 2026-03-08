513edeecd71140596f8ac5647fe53f4f21bee8a1
Lucy <244015723+ellicul@users.noreply.github.com>
2026-03-08 18:29:27 +0000

    Fix typo in dedicated_server help message

--- handheld/src/main_dedicated.cpp
+++ handheld/src/main_dedicated.cpp
@@ -31,7 +31,7 @@ int main(int numArguments, char* pszArgs[]) {
 	ArgumentsSettings aSettings(numArguments, pszArgs);
 	if(aSettings.getShowHelp()) {
 		ArgumentsSettings defaultSettings(0, NULL);
-		printf("Minecraft Pockect Edition Server %s\n", Common::getGameVersionString("").c_str());
+		printf("Minecraft Pocket Edition Server %s\n", Common::getGameVersionString("").c_str());
 		printf("-------------------------------------------------------\n");
 		printf("--cachepath - Path to where the server can store temp stuff (not sure if this is used) [default: \"%s\"]\n", defaultSettings.getCachePath().c_str());
 		printf("--externalpath - The path to the place where the server should store the levels. [default: \"%s\"]\n", defaultSettings.getExternalPath().c_str());
